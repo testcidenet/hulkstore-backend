918e7dcb2a2be029f0fb07adb2971c005913e86a
cafazual <cafazual30@gmail.com>
2020-04-30 12:51:19 -0500

    New updates relation with crossOrigin

--- src/main/java/com/testcidenet/hulkstore/Application.java
+++ src/main/java/com/testcidenet/hulkstore/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.testcidenet.hulkstore", "com.testcidenet.controller", "com.testcidenet.model","com.testcidenet.repository"})
 public class Application {
 
 	public static void main(String[] args) {
