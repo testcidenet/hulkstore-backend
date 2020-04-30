4be27a9b877ca052be924c729a90db15016f83d1
cafazual <cafazual30@gmail.com>
2020-04-30 12:38:26 -0500

    ComponentScan update test

--- src/main/java/com/testcidenet/hulkstore/Application.java
+++ src/main/java/com/testcidenet/hulkstore/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.testcidenet"})
+@ComponentScan(basePackages={"com.testcidenet.hulkstore", "controller", "model","repository"})
 public class Application {
 
 	public static void main(String[] args) {
