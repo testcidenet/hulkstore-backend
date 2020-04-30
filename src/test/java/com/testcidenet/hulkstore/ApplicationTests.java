fb40cff40f66b977b67358b50013ee3224fa62fb
cafazual <cafazual30@gmail.com>
2020-04-30 12:16:23 -0500

    Updates in Pagination

--- src/test/java/com/testcidenet/hulkstore/ApplicationTests.java
+++ src/test/java/com/testcidenet/hulkstore/ApplicationTests.java
@@ -1,10 +1,13 @@
 package com.testcidenet.hulkstore;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DemoApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
